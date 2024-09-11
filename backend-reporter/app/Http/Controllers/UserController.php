c262ced90b5bd7427acd31aa98a61d135501eab7
jzorgenfreijs <sipols13@gmail.com>
2024-09-11 21:51:07 +0300

    Disabled CSRF protections, setup basic user controller.

---
+++ backend-reporter/app/Http/Controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed', // Frontend request needs to have field: 'password_confirmation'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+        $request->session()->regenerate();
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        // Regenerate the session to prevent fixation attacks
+        $request->session()->regenerate();
+
+        return response()->json(['message' => 'Login successful', 'user' => Auth::user()], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(['message' => 'Logout successful'], 200);
+    }
+
+    public function getUser(Request $request)
+    {
+        return response()->json(['user' => Auth::user()], 200);
+    }
+
+    public function test()
+    {
+        return response()->json([
+            'message' => 'test!',
+        ]);
+    }
+}
