71327a4d54bd1c9958d9dfe086f152e38d85d6e3
jzorgenfreijs <sipols13@gmail.com>
2024-09-12 22:27:54 +0300

    Created vacancies list sketch.

--- frontend-reporter/src/router/routes.ts
+++ frontend-reporter/src/router/routes.ts
@@ -1,8 +1,10 @@
 import HomeView from '../views/HomeView.vue'
+import VacanciesList from '../views/VacanciesList.vue'
 
 const routes = [ 
     { path: '/', name: 'home', component: HomeView },
-    { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') }
+    { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
+    { path: '/vacancies', name: 'vacancies', component:  VacanciesList },
 ]
 
 export default routes;
--- frontend-reporter/tailwind.config.js
+++ frontend-reporter/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {},
