1beaecf7087f18834810547c17de2ab1719da44f
Evgeniy Kurshev <evgeniy.kurshev@upcode.lv>
2024-09-13 22:13:32 +0300

    handling invalid uri

--- frontend-reporter/src/router/routes.ts
+++ frontend-reporter/src/router/routes.ts
@@ -1,10 +1,12 @@
 import HomeView from '../views/HomeView.vue'
 import VacanciesList from '../views/VacanciesList.vue'
+import NotFound from "@/views/errors/NotFoundView.vue";
 
 const routes = [ 
     { path: '/', name: 'home', component: HomeView },
     { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
     { path: '/vacancies', name: 'vacancies', component:  VacanciesList },
+    { path: '/:catchAll(.*)', name: 'NotFound', component: NotFound}
 ]
 
 export default routes;
---
+++ frontend-reporter/src/views/errors/NotFoundView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import Button from 'primevue/button';
+</script>
+
+<template>
+  <h1>404</h1>
+  
+  <RouterLink to="/">
+    <Button label="Go Back" icon="pi pi-angle-left" />
+  </RouterLink>
+</template>
