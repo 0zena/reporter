aafd401b4de4ccfb51a1ee8f208ecb6fa5378214
monstrs <sipols13@gmail.com>
2024-09-18 10:50:00 +0300

    Created database migrations

--- backend-reporter/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend-reporter/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->boolean('is-admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
