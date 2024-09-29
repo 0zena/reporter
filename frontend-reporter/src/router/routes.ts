1e179e1fd8dd89d77eaf8ac3ae8c827b11a7714a
21DP3Ekaln <13dpekalnins@rvt.lv>
2024-09-29 22:22:52 +0300

    Home Page

--- frontend-reporter/src/router/routes.ts
+++ frontend-reporter/src/router/routes.ts
@@ -1,12 +1,13 @@
 import HomeView from '../views/HomeView.vue'
 import VacanciesList from '../views/VacanciesList.vue'
 import NotFound from "@/views/errors/NotFoundView.vue";
-
+import HomePage from "@/views/HomePage.vue"
 const routes = [ 
     { path: '/', name: 'home', component: HomeView },
     { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
     { path: '/vacancies', name: 'vacancies', component:  VacanciesList },
-    { path: '/:catchAll(.*)', name: 'NotFound', component: NotFound}
+    { path: '/:catchAll(.*)', name: 'NotFound', component: NotFound},
+    { path: '/homepage', name: 'homepage', component:  HomePage },
 ]
 
 export default routes;
