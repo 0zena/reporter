8fdd2831c5e6e1709f20a5167ff7059c37b738d2
jzorgenfreijs <sipols13@gmail.com>
2024-10-16 17:38:44 +0300

    Created basic vacancy listing component.

--- frontend-reporter/src/views/VacanciesList.vue
+++ frontend-reporter/src/views/VacanciesList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import NavigationBar from '../components/navigationbar/NavBar.vue';
+import VacancySmallListing from '@/components/VacancySmallListing.vue';
 
 import Button from 'primevue/button';
 import { useRouter } from 'vue-router';
@@ -14,20 +15,13 @@ const goToEditor = () => {
 <template>
   <div class="w-full min-h-screen h-auto bg-zinc-500">
     <NavigationBar></NavigationBar>
-     <div id="main" class="w-full h-auto flex flex-row-reverse">
-      <div id="menu-placeholder" class="bg-zinc-400 w-[350px] h-auto"></div>
+     <div id="main" class="w-full h-auto min-h-full flex flex-row-reverse">
+      <div id="menu-placeholder" class="bg-zinc-400 w-[350px] h-auto min-h-full"></div>
 
       <Button icon="pi pi-plus" class="h-10 m-5" style="width: 3rem;" @click="goToEditor"/>
 
       <div id="listing-wrapper" class="w-full h-full flex flex-col justify-center items-center pt-[75px] pb-[50px]">
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
-        <div id="listing-placeholder" class="w-3/5 h-[100px] bg-zinc-100 m-[25px] rounded-md"></div>
+        <VacancySmallListing image="http://127.0.0.1:8000/storage/vacancy_images/prYCWmKFdRPFVsa015ow7xqoueNNLe0Pc9UWAcD5.jpg" title="InnovateX - Senior Full Stack Developer" category="IT - Programming" @click="console.log('test')"/>
       </div>
 
     </div>
