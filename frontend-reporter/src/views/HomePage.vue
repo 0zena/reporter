79b0bcfb7f7f2c27b48990b0582b51f165fa28b3
LeprechaunC <vitolins.heinrihs@inbox.lv>
2024-10-21 18:51:41 +0300

    small changes regarding front end LANDING PAGE

--- frontend-reporter/src/views/HomePage.vue
+++ frontend-reporter/src/views/HomePage.vue
@@ -9,12 +9,8 @@
         <p class="mt-6 text-lg leading-8 text-gray-600">
           Search through a variety of job listings across different categories.
         </p>
-        <div class="mt-10 flex items-center justify-center gap-x-6">
-          <router-link
-            to="/login"
-            class="rounded-md"
-            style="background-color: #059669; color: white; padding: 0.625rem 1rem; font-size: 0.875rem; font-weight: 600; text-align: center; border-radius: 0.375rem; box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);"
-          >
+        <div class="p-button mt-10 flex items-center justify-center gap-x-6">
+          <router-link to="/login">
             Get Started
           </router-link>
         </div>
@@ -22,12 +18,12 @@
     </div>
   </div>
 
-  <!-- First Section - Text Left, Image Right -->
+
   <section class="mx-auto my-16 flex flex-col lg:flex-row items-center justify-between max-w-7xl px-6">
     <div class="w-full lg:w-1/2 lg:pr-8 mb-8 lg:mb-0">
       <h1 class="text-3xl font-bold text-white mb-4">Efficient Way to Get a Job!</h1>
       <p class="text-lg text-white">
-        Discover your dream job with ease! Whether you're starting your career or looking to level up, 
+        Discover your dream job with ease! Whether you're starting your career or looking to advance, 
         we provide tools to search through various listings, apply with ease, and get job alerts tailored 
         to your needs.
       </p>
@@ -41,8 +37,8 @@
     </div>
   </section>
 
-  <!-- Second Section - Image Left, Text Right -->
-  <section class="mx-auto my-16 flex flex-col-reverse lg:flex-row items-center justify-between max-w-7xl px-6">
+
+  <section class="mx-auto my-16 flex flex-col-reverse lg:flex-row items-center justify-between max-w-7xl px-6 mb-40">
     <div class="w-full lg:w-1/2 lg:pr-8 mt-8 lg:mt-0">
       <img
         src="@/assets/overview1.jpg"
@@ -51,10 +47,10 @@
       />
     </div>
     <div class="w-full lg:w-1/2 lg:pl-8">
-      <h1 class="text-3xl font-bold text-white mb-4">Advanced Search and Alerts</h1>
+      <h1 class="text-3xl font-bold text-white mb-4">Advanced Search</h1>
       <p class="text-lg text-white">
         Filter through job listings with advanced search options to find exactly what you're looking for. 
-        Set up custom alerts to be notified the moment a job that fits your criteria is posted.
+        Favorite your vacancies to keep them organized, and keep yourself updated with the latest vacancies!
       </p>
     </div>
   </section>
