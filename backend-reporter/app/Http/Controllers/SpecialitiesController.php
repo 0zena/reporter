d93f52d55174687832b4a26278dc30123471a15a
21DP3Ekaln <13dpekalnins@rvt.lv>
2024-10-22 00:40:33 +0300

    Getting rid of old code

--- backend-reporter/app/Http/Controllers/SpecialitiesController.php
+++ backend-reporter/app/Http/Controllers/SpecialitiesController.php
@@ -10,8 +10,8 @@ class SpecialitiesController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'category_id' => 'required|exists:categories,id',
+            'name' => 'required|string|max:255', // Validate speciality name
+            'category_id' => 'required|exists:categories,id', // Validate category ID
         ]);
 
         $speciality = Speciality::create([
@@ -19,18 +19,7 @@ class SpecialitiesController extends Controller
             'category_id' => $request->input('category_id'),
         ]);
 
-        return response()->json($speciality, 201);
-    }
-
-    public function getByCategory($categoryId)
-    {
-        $specialities = Speciality::where('category_id', $categoryId)->get();
-
-        if ($specialities->isEmpty()) {
-            return response()->json(['message' => 'No specialities found for this category'], 404);
-        }
-
-        return response()->json($specialities, 200);
+        return response()->json($speciality, 201); // Return the created speciality with a 201 status code
     }
 
     public function getByCategory($categoryId)
